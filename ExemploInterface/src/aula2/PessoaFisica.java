ac9d4107b9b4d02099cd98c7bcf90ad581cec960
Berinjah <lucas.silva2808@etec.sp.gov.br>
2018-07-05 11:46:18 -0300

    Exemplo de Interface

---
+++ ExemploInterface/src/aula2/PessoaFisica.java
@@ -0,0 +1,24 @@
+package aula2;
+
+public class PessoaFisica extends Pessoa {
+
+	private String nome;
+	
+	@Override
+	public String getNome() {
+		return nome;
+	}
+	
+	@Override
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public static void main(String[] args) {
+		PessoaFisica pessoa = new PessoaFisica();
+		pessoa.setNome("Lucas");
+		pessoa.getNome();
+
+	}
+
+}
